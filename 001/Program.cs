bb5431ac97f6d3f07e5bde7c8031f8b238dcbe0e
Gutnik <cipoga@gmail.com>
2023-07-08 17:43:19 +0300

    final 001

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int Prompt (string message)
+{
+    System.Console.Write (message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+int number = Prompt ("Введите трехзначное число: ");
+
+Console.WriteLine($"Введеное число: {number}");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра: {secondRank}");
